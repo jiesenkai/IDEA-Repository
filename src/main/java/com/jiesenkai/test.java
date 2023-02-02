e49908fc09a78a31a60296c35c8b673aa20a583c
小凯 <114136572+jiesenkai@users.noreply.github.com>
2023-02-02 16:33:21 +0800

    by jiesenkai commit

--- src/main/java/com/jiesenkai/test.java
+++ src/main/java/com/jiesenkai/test.java
@@ -6,11 +6,15 @@ public class test {
         System.out.println("修复bug");
         sayHello();
         service();
+        ByJisenkaiSendNode();
     }
     public static void sayHello(){
         System.out.println("hello git");
     }
     public static void service(){
         System.out.println("业务处理代码");
+    }
+    public sattic void ByJiesenkaiSendNode(){
+        System.out.println("来自jiesenkai提交的代码")
     }
 }
