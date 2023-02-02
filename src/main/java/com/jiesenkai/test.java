13a08827878aa4efa56450cf38aac696d9dd8e8b
jiesenkai <jiesenkai@qq.com>
2023-02-02 16:22:51 +0800

    bug还未修复 添加了一个业务处理代码

--- src/main/java/com/jiesenkai/test.java
+++ src/main/java/com/jiesenkai/test.java
@@ -5,9 +5,12 @@ public class test {
         System.out.println(1);
         System.out.println("写了个bug");
         sayHello();
+        service();
     }
     public static void sayHello(){
         System.out.println("hello git");
     }
-
+    public static void service(){
+        System.out.println("业务处理代码");
+    }
 }
