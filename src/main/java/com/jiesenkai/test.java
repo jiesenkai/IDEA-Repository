7a2db832c287425f66442e7318c541df9d83ff46
jiesenkai <jiesenkai@qq.com>
2023-02-02 16:11:08 +0800

    写了个sayHello方法 并且发现了一个bug

---
+++ src/main/java/com/jiesenkai/test.java
@@ -0,0 +1,13 @@
+package com.jiesenkai;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println("写了个bug");
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("hello git");
+    }
+
+}
